a89430b8f52979bf6dbea04b09141f86d3526546
Jacob Shirley <31049182+Term1984@users.noreply.github.com>
2020-11-03 14:23:31 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace SimpleWebBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.ToolStrip = new System.Windows.Forms.MenuStrip();
+            this.filesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bookmakrsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.AddressBar = new System.Windows.Forms.TextBox();
+            this.WebContainer = new CefSharp.WinForms.ChromiumWebBrowser();
+            this.BackButton = new System.Windows.Forms.Button();
+            this.ForwardButton = new System.Windows.Forms.Button();
+            this.NavButton = new System.Windows.Forms.Button();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.ToolStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ToolStrip
+            // 
+            this.ToolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.filesToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.bookmakrsToolStripMenuItem,
+            this.toolsToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.ToolStrip.Location = new System.Drawing.Point(0, 0);
+            this.ToolStrip.Name = "ToolStrip";
+            this.ToolStrip.Size = new System.Drawing.Size(800, 24);
+            this.ToolStrip.TabIndex = 0;
+            this.ToolStrip.Text = "menuStrip1";
+            // 
+            // filesToolStripMenuItem
+            // 
+            this.filesToolStripMenuItem.Name = "filesToolStripMenuItem";
+            this.filesToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.filesToolStripMenuItem.Text = "File";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // bookmakrsToolStripMenuItem
+            // 
+            this.bookmakrsToolStripMenuItem.Name = "bookmakrsToolStripMenuItem";
+            this.bookmakrsToolStripMenuItem.Size = new System.Drawing.Size(78, 20);
+            this.bookmakrsToolStripMenuItem.Text = "Bookmarks";
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.toolsToolStripMenuItem.Text = "Tools";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // AddressBar
+            // 
+            this.AddressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.AddressBar.Location = new System.Drawing.Point(12, 27);
+            this.AddressBar.Name = "AddressBar";
+            this.AddressBar.Size = new System.Drawing.Size(680, 20);
+            this.AddressBar.TabIndex = 1;
+            this.AddressBar.Text = "https://";
+            // 
+            // WebContainer
+            // 
+            this.WebContainer.ActivateBrowserOnCreation = false;
+            this.WebContainer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.WebContainer.Location = new System.Drawing.Point(0, 54);
+            this.WebContainer.Name = "WebContainer";
+            this.WebContainer.Size = new System.Drawing.Size(800, 393);
+            this.WebContainer.TabIndex = 2;
+            this.WebContainer.LoadingStateChanged += new System.EventHandler<CefSharp.LoadingStateChangedEventArgs>(this.WebContainer_LoadingStateChanged);
+            // 
+            // BackButton
+            // 
+            this.BackButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BackButton.Location = new System.Drawing.Point(698, 26);
+            this.BackButton.Name = "BackButton";
+            this.BackButton.Size = new System.Drawing.Size(25, 21);
+            this.BackButton.TabIndex = 3;
+            this.BackButton.Text = "<";
+            this.BackButton.UseVisualStyleBackColor = true;
+            this.BackButton.Click += new System.EventHandler(this.BackButton_Click);
+            // 
+            // ForwardButton
+            // 
+            this.ForwardButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ForwardButton.Location = new System.Drawing.Point(720, 26);
+            this.ForwardButton.Name = "ForwardButton";
+            this.ForwardButton.Size = new System.Drawing.Size(27, 21);
+            this.ForwardButton.TabIndex = 4;
+            this.ForwardButton.Text = ">";
+            this.ForwardButton.UseVisualStyleBackColor = true;
+            this.ForwardButton.Click += new System.EventHandler(this.ForwardButton_Click);
+            // 
+            // NavButton
+            // 
+            this.NavButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.NavButton.Location = new System.Drawing.Point(753, 26);
+            this.NavButton.Name = "NavButton";
+            this.NavButton.Size = new System.Drawing.Size(35, 21);
+            this.NavButton.TabIndex = 5;
+            this.NavButton.Text = "Go";
+            this.NavButton.UseVisualStyleBackColor = true;
+            this.NavButton.Click += new System.EventHandler(this.NavButton_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.NavButton);
+            this.Controls.Add(this.ForwardButton);
+            this.Controls.Add(this.BackButton);
+            this.Controls.Add(this.WebContainer);
+            this.Controls.Add(this.AddressBar);
+            this.Controls.Add(this.ToolStrip);
+            this.MainMenuStrip = this.ToolStrip;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ToolStrip.ResumeLayout(false);
+            this.ToolStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.MenuStrip ToolStrip;
+        private System.Windows.Forms.ToolStripMenuItem filesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bookmakrsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.TextBox AddressBar;
+        private CefSharp.WinForms.ChromiumWebBrowser WebContainer;
+        private System.Windows.Forms.Button BackButton;
+        private System.Windows.Forms.Button ForwardButton;
+        private System.Windows.Forms.Button NavButton;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+// Namespace
+namespace SimpleWebBrowser
+{
+    // Class
+    public partial class Form1 : Form
+    {
+        // Main Windows Form
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        // Core navigation function
+        private void NavigateToPage() 
+        {
+            AddressBar.Enabled = false;
+            NavButton.Enabled = false;
+            WebContainer.Load(AddressBar.Text);
+            WebContainer.SetZoomLevel(-6.0);
+        }
+
+        // Browser back button
+        private void BackButton_Click(object sender, EventArgs e)
+        {
+            WebContainer.Back();
+        }
+
+        // Browser forward button
+        private void ForwardButton_Click(object sender, EventArgs e)
+        {
+            WebContainer.Forward();
+        }
+        
+        // Browser navigation button
+        private void NavButton_Click(object sender, EventArgs e)
+        {
+            NavigateToPage();
+        }
+
+        // Loading state changed
+        private void WebContainer_LoadingStateChanged(object sender, LoadingStateChangedEventArgs e)
+        {
+            WebContainer.SetZoomLevel(-1.0);
+            AddressBar.Enabled = true;
+            NavButton.Enabled = true;
+
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            String AppInfo = "Simple Web Browser v1.0.0-alpha1";
+            MessageBox.Show(AppInfo);
+        }
+    }
+}
